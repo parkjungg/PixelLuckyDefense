f12ca4b35cd70216c7b304ab96af12d601309725
pjk3665@naver.com <pjk3665@naver.com>
2024-11-09 19:24:57 +0900

    도움말 추가

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,10 +1,10 @@
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
     public GameObject canvas1;
+    public GameObject canvas2;
     public void StartGame() {
         SceneManager.LoadScene("MainScene");
     }
@@ -16,5 +16,11 @@ public class GameStart : MonoBehaviour
     }
     public void TurnOffCanvas1() {
         canvas1.SetActive(false);        
+    }
+    public void TurnOnCanvas2() {
+        canvas2.SetActive(true);
+    }
+    public void TurnOffCanvas2() {
+        canvas2.SetActive(false);        
     }
 }
